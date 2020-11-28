bd3dbc3c0c66c70a716b5ac4b14ef3613419b51c
Anıl Şenay <anilsenay3@gmail.com>
2020-11-28 23:03:39 +0300

    Update assingment adding

--- RandomBot.java
+++ RandomBot.java
@@ -1,13 +1,11 @@
 import java.util.ArrayList;
-import java.util.Date;
 
 public class RandomBot extends User implements Assign {
 
     Dataset dataset;
 
-    public RandomBot(String userName, int userID, String userType, Dataset dataset) {
+    public RandomBot(String userName, int userID, String userType) {
         super(userName, userID, userType);
-        this.dataset = dataset;
     }
 
     public RandomBot() {
@@ -17,23 +15,24 @@ public class RandomBot extends User implements Assign {
     public RandomBot(Dataset dataset) {
         this.dataset = dataset;
     }
-    //Method below assigns labels to an instance by maximum of maxLabelPerInstance
+
+    // Method below assigns labels to an instance by maximum of maxLabelPerInstance
     // then returns to assignmentList in dataset
     @Override
     public void assign(Dataset dataset, Instance instance) {
-        Date dateTime = new Date();
-        //getting a random value between 1 and max labels per instance
-        int maxLabelRandom= (int) (1+ (Math.random() * dataset.maxLabelPerInstance));
 
-            ArrayList<Label> labels= new ArrayList<Label>(); //creates a local label arraylist to store labels to assign
-            //Chooses a random label from classLabel arraylist in dataset
-            for (int j=0; j< maxLabelRandom; j++){
-            Label getRandomLabel = dataset.classLabels.get((int)((Math.random() * dataset.classLabels.size())));
+        // getting a random value between 1 and max labels per instance
+        int maxLabelRandom = (int) (1 + (Math.random() * dataset.maxLabelPerInstance));
+
+        ArrayList<Label> labels = new ArrayList<Label>(); // creates a local label arraylist to store labels to assign
+        // Chooses a random label from classLabel arraylist in dataset
+        for (int j = 0; j < maxLabelRandom; j++) {
+            Label getRandomLabel = dataset.classLabels.get((int) ((Math.random() * dataset.classLabels.size())));
             labels.add(getRandomLabel);
-            }
-            //returns to assignmentList in dataset
-            Assignment assignment = new Assignment(instance, this, labels);
-            dataset.assignmentList.add(assignment);
+        }
+        // returns to assignmentList in dataset
+        Assignment assignment = new Assignment(instance, this, labels);
+        dataset.addAssignment(assignment);
 
     }
 
