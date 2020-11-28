9f9db99b994cf39a52d045b03a402d2d545d682b
Anıl Şenay <anilsenay3@gmail.com>
2020-11-28 20:48:58 +0300

    Create Dataset, Label and Instance objects

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.ArrayList;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -6,68 +6,80 @@ import java.io.FileReader;
 import java.util.Iterator;
 
 public class Main {
-  public static void main (String args[]) {
-      
+    public static void main(String args[]) {
 
-    JSONParser parser = new JSONParser();
+        Dataset dataset = null;
+        JSONParser parser = new JSONParser();
 
-    try {
-        Object obj = parser.parse(new FileReader("config.json"));
+        try {
+            Object obj = parser.parse(new FileReader("config.json"));
 
-        JSONObject jsonObject = (JSONObject) obj;
+            JSONObject jsonObject = (JSONObject) obj;
 
-        JSONArray userList = (JSONArray) jsonObject.get("users");
+            JSONArray userList = (JSONArray) jsonObject.get("users");
 
-        Iterator<JSONObject> userListIterator = userList.iterator();
+            Iterator<JSONObject> userListIterator = userList.iterator();
 
-        while (userListIterator.hasNext()) {
-            JSONObject userObj = (userListIterator.next());
-            System.out.println(userObj.get("user name")); 
-            System.out.println(userObj.get("user id"));
-            System.out.println(userObj.get("user type"));
-        }
-    
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
-
-    try {
-        Object obj = parser.parse(new FileReader("input.json"));
-
-        JSONObject jsonObject = (JSONObject) obj;
-        
-        String datasetName = (String) jsonObject.get("dataset name");
-        System.out.println(datasetName);
-
-        Long maxNumberPerInstance = (Long) jsonObject.get("maximum number of labels per instance");
-        System.out.println(maxNumberPerInstance);
-
-        JSONArray classLabelList = (JSONArray) jsonObject.get("class labels");
-        JSONArray instanceList = (JSONArray) jsonObject.get("instances");
-
-        Iterator<JSONObject> instanceIterator = instanceList.iterator();
-        Iterator<JSONObject> labelIterator = classLabelList.iterator();
-
-        while (instanceIterator.hasNext()) {
-            JSONObject instanceObj = (instanceIterator.next());
-            System.out.println(instanceObj.get("instance")); 
-            System.out.println(instanceObj.get("id"));
+            while (userListIterator.hasNext()) {
+                JSONObject userObj = (userListIterator.next());
+                System.out.println(userObj.get("user name"));
+                System.out.println(userObj.get("user id"));
+                System.out.println(userObj.get("user type"));
+                // create user
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        while (labelIterator.hasNext()) {
-            JSONObject labelObj = (labelIterator.next());
-            System.out.println(labelObj.get("label text")); 
-            System.out.println(labelObj.get("label id"));
+        try {
+            Object obj = parser.parse(new FileReader("input.json"));
+
+            JSONObject jsonObject = (JSONObject) obj;
+
+            int datasetId = ((Long) jsonObject.get("dataset id")).intValue();
+            System.out.println(datasetId);
+
+            String datasetName = (String) jsonObject.get("dataset name");
+            System.out.println(datasetName);
+
+            int maxNumberPerInstance = ((Long) jsonObject.get("maximum number of labels per instance")).intValue();
+            System.out.println(maxNumberPerInstance);
+
+            dataset = new Dataset(datasetId, datasetName, maxNumberPerInstance);
+
+            JSONArray classLabelList = (JSONArray) jsonObject.get("class labels");
+            JSONArray instanceList = (JSONArray) jsonObject.get("instances");
+
+            @SuppressWarnings("unchecked")
+            Iterator<JSONObject> instanceIterator = instanceList.iterator();
+            @SuppressWarnings("unchecked")
+            Iterator<JSONObject> labelIterator = classLabelList.iterator();
+
+            while (instanceIterator.hasNext()) {
+                JSONObject instanceObj = (instanceIterator.next());
+                dataset.addInstance(
+                        new Instance((String) instanceObj.get("instance"), ((Long) instanceObj.get("id")).intValue()));
+            }
+
+            while (labelIterator.hasNext()) {
+                JSONObject labelObj = (labelIterator.next());
+                dataset.addLabel(
+                        new Label(((Long) labelObj.get("label id")).intValue(), (String) labelObj.get("label text")));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-    } catch (Exception e) {
-        e.printStackTrace();
+        System.out.println("Dataset id : " + dataset.getDatasetID());
+        ArrayList<Instance> instanceList = dataset.getInstances();
+        ArrayList<Label> labelList = dataset.getClassLabels();
+
+        for (int i = 0; i < instanceList.size(); i++) {
+            System.out.println(instanceList.get(i).getContent());
+        }
+        for (int i = 0; i < labelList.size(); i++) {
+            System.out.println(labelList.get(i).getLabelName());
+        }
     }
 }
-
-
-
-
-
- }
-    
