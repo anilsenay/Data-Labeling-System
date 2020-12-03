b1b8afadddaba27670d8679479e409a5348d04d9
ElburuzGurbuz <65295748+ElburuzGurbuz@users.noreply.github.com>
2020-12-04 02:19:07 +0300

    Add comments to RandomLabelingMechanism.java

--- RandomLabelingMechanism.java
+++ RandomLabelingMechanism.java
@@ -1,15 +1,19 @@
 import java.util.ArrayList;
 import java.util.Date;
 
+/* RandomLabelingMechanism is kind of labelingMechanism which assignment process handled randomly, 
+   inherits from LabelingMechanism class. Implemented Singleton Pattern. */  
 public class RandomLabelingMechanism extends LabelingMechanism
 {
 	private static RandomLabelingMechanism randomLabelingMechanism;
-	
+    
+    // No arg constructor.
 	private RandomLabelingMechanism() {
 		
 	 }
-	
-	public static synchronized RandomLabelingMechanism getInstance() { // getInstance method for singleton pattern
+    
+    // getInstance method for singleton pattern.
+	public static synchronized RandomLabelingMechanism getInstance() { 
 
 	    if (randomLabelingMechanism == null) {
 
@@ -17,28 +21,31 @@ public class RandomLabelingMechanism extends LabelingMechanism
 	    }
 
 	    return randomLabelingMechanism;
-	  }
-	// Method below assigns labels to an instance by maximum of maxLabelPerInstance
-    // then returns to assignmentList in dataset
+      }
+      
+	/* Method below assigns labels to an instance by maximum of maxLabelPerInstance.
+       then returns to assignmentList in dataset. */
     public void assign(Dataset dataset, Instance instance,User user) {
 
-        // getting a random value between 1 and max labels per instance
+        // Getting a random value between 1 and max labels per instance.
         int maxLabelRandom = (int) (1 + (Math.random() * dataset.getMaxLabelPerInstance()));
+        
+        // Creates a local label arraylist to store labels to assign.
+        ArrayList<Label> labels = new ArrayList<Label>(); 
 
-        ArrayList<Label> labels = new ArrayList<Label>(); // creates a local label arraylist to store labels to assign
-        // Chooses a random label from classLabel arraylist in dataset
+        // Chooses a random label from classLabel arraylist in dataset.
         for (int j = 0; j < maxLabelRandom; j++) {
-            Label getRandomLabel = dataset.getClassLabels()
-                    .get((int) ((Math.random() * dataset.getClassLabels().size())));
+            Label getRandomLabel = dataset.getClassLabels().get((int) ((Math.random() * dataset.getClassLabels().size())));
             labels.add(getRandomLabel);
-
+            
+            // Print the created assignment to the log file.
             Logger.getInstance().print(new Date(),
                     "[Assignment] INFO user id:" + user.getUserID() + " " + user.getUserName() + " tagged instance id:"
                             + instance.getInstanceID() + " " + "with class label " + getRandomLabel.getLabelID() + ":"
                             + getRandomLabel.getLabelName() + " " + " instance: \"" + instance.getContent() + "\"");
         }
 
-        // returns to assignmentList in dataset
+        // Returns to assignmentList in dataset.
         Assignment assignment = new Assignment(instance, user, labels);
         dataset.addAssignment(assignment);
 
