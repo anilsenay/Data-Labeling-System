baf52f7a7e2a26ad0593718266b0de23fa673eb9
Ahmet Önkol <onkoll.ahmet@gmail.com>
2020-12-03 23:41:01 +0300

    Update RandomLabelingMechanism.java

--- RandomLabelingMechanism.java
+++ RandomLabelingMechanism.java
@@ -21,9 +21,14 @@ public class RandomLabelingMechanism extends LabelingMechanism
 	// Method below assigns labels to an instance by maximum of maxLabelPerInstance
     // then returns to assignmentList in dataset
     public void assign(Dataset dataset, Instance instance,User user) {
-
+        int maxLabelRandom=dataset.getMaxLabelPerInstance();
         // getting a random value between 1 and max labels per instance
-        int maxLabelRandom = (int) (1 + (Math.random() * dataset.getMaxLabelPerInstance()));
+        if (maxLabelRandom<1) {
+            System.err.println("Max Label Per Instance is not valid (less than 1)");
+            System.exit(1);
+        }
+        else { maxLabelRandom = (int) (1 + (Math.random() * dataset.getMaxLabelPerInstance()));}
+
 
         ArrayList<Label> labels = new ArrayList<Label>(); // creates a local label arraylist to store labels to assign
         // Chooses a random label from classLabel arraylist in dataset
@@ -43,5 +48,6 @@ public class RandomLabelingMechanism extends LabelingMechanism
         dataset.addAssignment(assignment);
 
     }
-	
 }
+
+
