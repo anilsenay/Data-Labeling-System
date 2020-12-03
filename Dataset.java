3f22fb12fc54e8afd788718c64869c7305242c00
ElburuzGurbuz <65295748+ElburuzGurbuz@users.noreply.github.com>
2020-12-04 00:35:30 +0300

    Add comments to Dataset.java

--- Dataset.java
+++ Dataset.java
@@ -1,18 +1,20 @@
 import java.util.ArrayList;
 import java.util.Date;
 
+// Dataset class for storing labels, instances, assignments and settings information about dataset.   
 public class Dataset {
+
 	// The variables datasetID, datasetName, maxLabelPerInstance are created.
 	private int datasetID;
 	private String datasetName;
 	private int maxLabelPerInstance;
-
+     
 	// ClassLabels, instances, assignmentList Lists are created.
 	private ArrayList<Label> classLabels = new ArrayList<Label>();
 	private ArrayList<Instance> instances = new ArrayList<Instance>();
 	private ArrayList<Assignment> assignmentList = new ArrayList<Assignment>();
 
-	// no arg constructor
+	// No arg constructor.
 	public Dataset() {
 		super();
 	}
@@ -23,26 +25,28 @@ public class Dataset {
 		this.datasetID = datasetID;
 		this.datasetName = datasetName;
 		this.maxLabelPerInstance = maxLabelPerInstance;
-
+		
+		// Print the created dataset file to the log file.
 		Logger.getInstance().print(new Date(),
 				"[Dataset] INFO dataset: created \"" + datasetName + "\" with id: " + datasetID);
 	}
 
-	// Adds instance object to instances array list
+	// Adds instance object to instances array list.
 	protected void addInstance(Instance instance) {
 		this.instances.add(instance);
 	}
 
-	// Adds label object to classLabels array list
+	// Adds label object to classLabels array list.
 	protected void addLabel(Label label) {
 		this.classLabels.add(label);
 	}
 
-	// Adds instance object to instances array list
+	// Adds instance object to instances array list.
 	protected void addAssignment(Assignment assingment) {
 		this.assignmentList.add(assingment);
 	}
-
+	
+	//Getter methods for Dataset class.
 	public int getDatasetID() {
 		return this.datasetID;
 	}
