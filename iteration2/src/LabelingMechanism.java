4c610090b73315b42bf58e6d65bbd9b5922abbbe
kursatacikgoz <kursatacikgoz@marun.edu.tr>
2020-12-17 21:28:10 +0300

    upload src

---
+++ iteration2/src/LabelingMechanism.java
@@ -0,0 +1,10 @@
+/* LabelingMechanism is a superclass of each labelingMechanism classes. 
+   Each class must have assign method for different labeling types to perform assignment (for future iterations). */
+public abstract class LabelingMechanism {
+
+	void assign() 
+	{
+		
+	}
+
+}
