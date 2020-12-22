9bf36d810736edd8fbcc26ac73bba7b937212d27
beyzaaydogan <beyzaaydogan026@gmail.com>
2020-12-22 19:16:22 +0300

    Update Main.java

--- iteration2/src/Main.java
+++ iteration2/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         // Read config file and create dataset.
         DataLabelingSystem DLS = new DataLabelingSystem();
         DLS.loadConfig("config.json"); // Get user from config.json.
-        DLS.handleDataset();
+        DLS.handleDataset(DLS.getOutputName());
 
         // Random assignment for iteration-1.
         ArrayList<Instance> instanceList = DLS.getDataset().getInstances();
@@ -20,12 +20,13 @@ public class Main {
             for (int j = 0; j < userList.size(); j++) {
                 if (userList.get(j).getUserType().equalsIgnoreCase("RandomBot")) {
                     RandomBot user = (RandomBot) userList.get(j);
-                    user.assign(dataset, instanceList.get(i));
+                    Assignment assignment = user.assign(dataset, instanceList.get(i));
                     // Print results to the console and log file.
                     DLS.writeOutputFile();
+                    ReportingMechanism.getInstance().updateReport(assignment);
                 }
             }
         }
-
+        DLS.writeOutputFile();
     }
 }
