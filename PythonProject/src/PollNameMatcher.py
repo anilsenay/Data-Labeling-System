9bbe05a93accd203a19b256e0283c60bfb3ebd86
mehmetaliyuksel <mehmetaliyuksel5@gmail.com>
2021-01-15 15:56:03 +0300

    Initilaze Classes

---
+++ PythonProject/src/PollNameMatcher.py
@@ -0,0 +1,10 @@
+class PollNameMatcher(Matcher):
+
+  def __init__(self):
+    super().__init__()
+
+  def match (self):
+     return 0  
+  
+  
+pass
---
+++ PythonProject/src/PollReader.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+pollReader = pd.read_excel('./Data/Edited.xlsx')
+# or using sheet index starting 0
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 20000)
+QuestionCounter = 0
+AnswerCounter = 0
+
+
+for i in range(len(pollReader
+.columns)):
+    if (i >= 4):
+        if(i % 2 == 0):
+            QuestionCounter += 1
+            pollReader
+            .rename( columns={"Unnamed: "+str(i) :"Question"+str(QuestionCounter-1)}, inplace=True )
+        else:
+            AnswerCounter += 1
+            pollReader
+            .rename( columns={"Unnamed: "+str(i) :"Answer"+str(AnswerCounter-1)}, inplace=True )
+
+
+pollReader = pollReader.replace(np.nan, "", regex=True)
+print(pollReader.loc[::])
