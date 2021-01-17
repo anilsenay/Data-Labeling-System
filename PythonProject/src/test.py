1bbafe759d166482f400538e99d40689aba47a6b
Ahmet Önkol <onkoll.ahmet@gmail.com>
2021-01-17 23:34:49 +0300

    unnecessary classes are deleted

--- PythonProject/src/test.py
+++
@@ -1,6 +0,0 @@
-from Student import Student
-from StudentAnswer import StudentAnswer
-
-student = Student(5,"ali", "mail","repeat")
-stdAns = StudentAnswer(student, "oluyommm", "A")
-print(stdAns.question)  
